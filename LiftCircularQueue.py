95b2a030354c4a6a466ee853db4bc615b4f08bd0
GitKoti235 <31142253+GitKoti235@users.noreply.github.com>
2018-03-28 15:15:14 +0530

    Manifest initial
    Lift circular queue representation
    which predicts where the lift is

---
+++ LiftCircularQueue.py
@@ -0,0 +1,35 @@
+"""
+
+want to figure out positions of lift and drift using pulley
+
+pulley lies on top in between
+maintaining same length of rope 
+entire space is divided into 
+
+
+5/5 - drift	rope - 5/5
+4/5	- empty	rope - 4/5
+3/5	- empty	rope - 3/5
+2/5	- empty	rope - 2/5
+1/5	- empty lift - 1/5
+
+drift empty empty empty empty
+rope  rope 	rope  rope  lift
+
+rope drift empty empty empty
+rope rope  rope  lift  empty 
+
+rope rope drift empty empty 
+rope rope  lift empty empty
+
+rope rope drift empty empty 
+rope rope  lift empty empty
+
+rope rope drift empty empty 
+rope rope  lift empty empty
+
+lift rope rope rope drift empty empty empty 
+ Identify the lift element
+ move each element aside
+ 
+"""
